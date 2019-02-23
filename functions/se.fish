1cbbec01dfb0f12cda034db427871cece581fbb6
specter119 <specter119@gmail.com>
2019-02-23 14:41:09 +0800

    update se again

--- functions/se.fish
+++ functions/se.fish
@@ -1,3 +1,3 @@
 function se -d "Creates GUI emacs connecting to client with sudo"
-  emacsclient -e '(find-file "/sudo::/etc/passwd")' -ta '' $argv
+  emacsclient -ta '' $argv -e '(find-file "/sudo::/etc/passwd")'
 end
