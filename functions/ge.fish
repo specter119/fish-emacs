c159119f43cecc4d5a366bcc3e41a43b8b935fa7
specter119 <specter119@gmail.com>
2019-02-28 15:57:55 +0800

    fix function names

--- functions/ge.fish
+++ functions/ge.fish
@@ -1,3 +1,3 @@
-function e -d "Opens in GUI, with non-daemon as alternate"
+function ge -d "Opens in GUI, with non-daemon as alternate"
   emacsclient -ca '' $argv
 end
--- functions/sge.fish
+++ functions/sge.fish
@@ -1,3 +1,3 @@
-function se -d "Creates GUI emacs connecting to client with sudo"
+function sge -d "Creates GUI emacs connecting to client with sudo"
   env SUDO_EDITOR='emacsclient -ca ""' sudo -e $argv
 end
